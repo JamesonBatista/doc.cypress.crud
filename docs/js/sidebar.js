5260d6544fd1c328df2c9919af2626a60c7b34c5
Jam Batista <batistadado@gmail.com>
2024-11-01 08:08:42 -0300

    project cypress-crud

--- docs/js/sidebar.js
+++ docs/js/sidebar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async function () {
-  return await fetch("./js/sidebar.js")
+  return await fetch("../sidebar.html")
   .then(response => response.text())
   .then(data => {
 	  // Insere o conteúdo carregado no contêiner do sidebar
